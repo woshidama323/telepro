fda14f2e0ab25ba0d95c2168244f3fd1bff0f170
root <root@ip-172-31-42-245.ap-southeast-1.compute.internal>
2018-09-03 04:06:19 +0000

    version1 of a bot to send lucky money

--- bot.py
+++ bot.py
@@ -17,13 +17,18 @@ LUCKYINTERVAL = 3600*3
 
 #全局队列
 gqueue = Queue()     #红包生成queue
-sumqueue = Queue()     #红包生成queue
+sumqueue = Queue()     #本轮剩余红包数量队列
+
+groupinfoque = Queue()
 # userqueue = queue.Queue() #本轮已经抢过红包队列
 
 #数据库读写
 from pymongo import MongoClient
 
-
+luckytextpre3 = "触币-基于区块链的数字资产交易平台。全民签到领CHU币活动中。\
+                每天9、12、18时准时发送红包，一共三次签到机会。签到已更新，大家快来签到吧！"
+luckytextpre = "触币-基于区块链的数字资产交易平台。全民签到领CHU币活动中（每天签到3次，9、12、18时准时更新签到）。\
+签到已更新，大家快来签到吧！"
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -41,6 +46,12 @@ def hello(bot,update):
     #bot.send_message(chat_id='@helloeasy', text = update.message.text)
     update.message.reply_text('Bye! I hope we can talk again some day.',
                               reply_markup=ReplyKeyboardRemove())
+
+def start(bot,update):
+    logger.info("starting the bot for getting the group info %s" % update)
+
+    if not groupinfoque.full():
+        groupinfoque.put(update.message.chat.id)
 
 def create(bot,update):
     print("hello update %s" % update)
@@ -87,23 +98,23 @@ def gethongbao(bot,query):
         
         luckyuser = db.posts.find_one({'userid':query.from_user.id})
 
-
+        luckychu = 0
+        sumleft = 0
         if luckyuser is None:
 
             #get the random 
             luckychu = gqueue.get()
-            sumleft = 0
+            
             if not sumqueue.empty():
                 sumleft = sumqueue.get()
                 sumleft -=  int(luckychu)
                 sumqueue.put(sumleft)
 
-            logger.info("current the sumleft is {}".format(sumleft))
             #----------------------------------------------------------
             print("hi {}, you are getting the locky money".format(query.from_user.first_name))
-            db.posts.insert_one({'userid':query.from_user.id,'chunum':luckychu,'isregister':False,'isGetCHU':True})
+            db.posts.insert_one({'userid':query.from_user.id,'chunum':luckychu,'luckynum':luckychu,'isregister':False,'isGetCHU':True})
 
-            bot.edit_message_text(text="hi {}, 你已经抢到过CHU币".format(query.from_user.first_name),
+            bot.edit_message_text(text="hi {} {}, 抢到{}个CHU币".format(query.from_user.first_name,query.from_user.last_name,int(luckychu)),
                     chat_id=query.message.chat_id,
                     message_id=query.message.message_id)
 
@@ -111,46 +122,35 @@ def gethongbao(bot,query):
 
             #get the random 
             luckychu = gqueue.get()
-            sumleft = 0
+            #left random lucky money
             if not sumqueue.empty():
                 sumleft = sumqueue.get()
                 sumleft -=  int(luckychu)
                 sumqueue.put(sumleft)
 
-            logger.info("current the sumleft is {}".format(sumleft))
             #----------------------------------------------------------
-            
+
             luckyuser['chunum'] += luckychu
+            luckyuser['luckynum'] = luckychu
             luckyuser['isGetCHU'] = True
             db.posts.replace_one({'userid':query.from_user.id},luckyuser)
             
             
             print("get the lucky coin : %s for %s " % (luckychu,query))
-            bot.edit_message_text(text="hi {}, 抢到{}个CHU币".format(query.from_user.first_name,int(luckychu)),
+            bot.edit_message_text(text="hi {} {}, 抢到{}个CHU币".format(query.from_user.first_name,query.from_user.last_name,int(luckychu)),
                             chat_id=query.message.chat_id,
                             message_id=query.message.message_id)
         else:
-            sum = 0 
-            # listforqueue = []
-            # while gqueue.qsize() != 0:
-                
-            #     logger.info("get the list is {}".format(listforqueue))
-            #     temprandom =  gqueue.get()
-            #     listforqueue.append(temprandom)
-            #     sum += temprandom
-            
-            # logger.info("get the list is {}".format(listforqueue))
-            # for i in listforqueue:
-            #     gqueue.put(i)
-
-            bot.edit_message_text(  text="hi {},你已抢过红包,已获得{}个CHU币,请3小时后再来,目前红包剩余{}/20，剩余CHU币{}个".format(query.from_user.first_name,int(luckyuser['chunum']), gqueue.qsize(), sumleft),
+            logger.info("current the sumleft is {}".format(sumleft))
+            gsumleft = sumqueue.get()
+            bot.edit_message_text(  text="hi {} {},你已抢过红包,已获得{}个CHU币,请3小时后再来,目前红包剩余{}/20，剩余CHU币{}个".format(query.from_user.first_name,query.from_user.last_name,int(luckyuser['luckynum']), gqueue.qsize(), gsumleft),
                         chat_id=query.message.chat_id,
                         message_id=query.message.message_id)       
-                        
+            sumqueue.put(gsumleft)
         client.close()
         
     else:
-        bot.edit_message_text(  text="hi {},当前不是发红包的时间，请3小时后再来".format(query.from_user.first_name),
+        bot.edit_message_text(  text="hi {} {},当前不是发红包的时间，请3小时后再来".format(query.from_user.first_name,query.from_user.last_name),
                         chat_id=query.message.chat_id,
                         message_id=query.message.message_id)
 
@@ -163,7 +163,7 @@ def checkin(bot,query):
     if useritem == None:
 
         db.posts.insert_one({'userid':query.from_user.id,'chunum':20,'isregister':True,'isGetCHU':False})
-        bot.edit_message_text(  text="hi {},签到成功,已获得20个CHU币".format(query.from_user.first_name),
+        bot.edit_message_text(  text="hi {} {},签到成功,已获得20个CHU币".format(query.from_user.first_name,query.from_user.last_name),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
 
@@ -173,11 +173,11 @@ def checkin(bot,query):
         useritem['isregister'] = True
         
         db.posts.replace_one({'userid':query.from_user.id},useritem)
-        bot.edit_message_text(  text="hi {},签到成功,已获得20个CHU币".format(query.from_user.first_name),
+        bot.edit_message_text(  text="hi {} {},签到成功,已获得20个CHU币".format(query.from_user.first_name,query.from_user.last_name),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
     else:
-        bot.edit_message_text(  text="hi {},今天已签过到".format(query.from_user.first_name),
+        bot.edit_message_text(  text="hi {} {},今天已签过到".format(query.from_user.first_name,query.from_user.last_name),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
 
@@ -189,15 +189,15 @@ def getthechuinfo(bot,query):
     infoitem = db.posts.find_one({'userid':query.from_user.id})
     
     if infoitem is None:
-        bot.edit_message_text(  text="hi {},未查到你任何信息，今天可以签到或者抢红包".format(query.from_user.first_name),
+        bot.edit_message_text(  text="hi {} {},未查到你任何信息，今天可以签到或者抢红包".format(query.from_user.first_name,query.from_user.last_name),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
     elif 'chunum' in infoitem:
-        bot.edit_message_text(  text="hi {},共获得chu币总数为:{}".format(query.from_user.first_name,int(infoitem['chunum'])),
+        bot.edit_message_text(  text="hi {} {},共获得chu币总数为:{}".format(query.from_user.first_name,query.from_user.last_name,int(infoitem['chunum'])),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
     else:
-        bot.edit_message_text(  text="hi {},未能查到任何信息，请与客服联系".format(query.from_user.first_name),
+        bot.edit_message_text(  text="hi {} {},未能查到任何信息，请与客服联系".format(query.from_user.first_name,query.from_user.last_name),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
     print("query is")
@@ -205,10 +205,10 @@ def getthechuinfo(bot,query):
 
 def generate_random_integers(_sum, n):  
     mean = _sum / n
-    variance = int(0.5 * mean)
+    variance = int(1 * mean)
 
     min_v = mean - variance
-    max_v = mean + variance
+    max_v = 200 #mean + variance
     array = [min_v] * n
 
     diff = _sum - min_v * n
@@ -225,8 +225,10 @@ def teleupdate():
     from telegram.ext import MessageHandler,Filters,CommandHandler,CallbackQueryHandler
     echo_handler = MessageHandler(Filters.text,hello)
     command_handler = CommandHandler("CHU币",create)
+    command_start_handler = CommandHandler("start",start) #
     dispatcher.add_handler(echo_handler)
     dispatcher.add_handler(command_handler)
+    dispatcher.add_handler(command_start_handler)
     dispatcher.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
@@ -242,12 +244,28 @@ def timerthread():
 def testprint():
     print("sending random number to the gqueue")
     import time
+    listforgroupid = []
     while True:
         time.sleep(300)  #LUCKYINTERVAL
 
-        # bot.edit_message_text(  text="hi {},今天已签过到".format(query.from_user.first_name),
+        #updater.bot.edit_message_text(  text="hi {},今天已签过到".format(query.from_user.first_name),
         #                         chat_id=query.message.chat_id,
         #                         message_id=query.message.message_id)
+        while groupinfoque.qsize() != 0:
+
+            groupchatid = groupinfoque.get()
+            listforgroupid.append(groupchatid)
+            logger.info("sending the info to chat {}".format(groupchatid))
+
+            for tmid in range(3):
+                logger.info("sending the info to chat {}".format(listforgroupid))
+                updater.bot.send_message(chat_id=groupchatid, text = luckytextpre)
+
+        for backtoqueue in listforgroupid:
+            groupinfoque.put(backtoqueue)
+            
+        listforgroupid.clear()
+            
                                 
         rannum = generate_random_integers(500,20)
 
@@ -263,6 +281,9 @@ def testprint():
             print("put random number into gqueue %s" % i)
             gqueue.put(i)
 
+        for gid in listforgroupid:
+            logger.info("group id is {}".format(gid))
+            groupinfoque.put(gid)
 
         while sumqueue.qsize() != 0:
             sumqueue.get()
@@ -270,6 +291,7 @@ def testprint():
         sumqueue.put(500)
 
 def dailylucky():
+    logger.info("starting change the database")
     client = MongoClient('localhost', 27017)
     db = client.test_database
     infoitem = db.posts.update_many({"isregister":True},{'$set':{"isregister":False}})
@@ -279,7 +301,11 @@ def runtimer():
     print("working on it", file=open("output.txt", "a"))
 
 def scheduler():
-    schedule.every().day.at("16:00").do(dailylucky)
+    schedule.every().day.at("01:00").do(dailylucky)
+    schedule.every().day.at("02:12").do(dailylucky)
+    schedule.every().day.at("04:00").do(dailylucky)
+    schedule.every().day.at("07:00").do(dailylucky)
+    
 
     while True:
         schedule.run_pending()
